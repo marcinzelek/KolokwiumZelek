0578edacc9904b1d6899bcfc38e7576e49624b5c
zelekmarcin <marcininho@gmail.com>
2017-06-19 15:13:50 +0200

    added class with 2 points

--- ZELEK/ZELEK/Program.cs
+++ ZELEK/ZELEK/Program.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace ZELEK
 {
-    class Program
+
+    class Punkty_dwa
+    {
+        public double X1, X2;
+        public double Y1, Y2;
+        public Punkty_dwa(double a1, double a2, double b1, double b2)
+        {
+            this.X1 = a1;
+            this.Y1 = b1;
+            this.X2 = a2;
+            this.Y2 = b2;
+        }
+    }
+}
+class Program
     {
         static void Main(string[] args)
         {
