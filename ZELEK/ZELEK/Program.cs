2a38f1d751fbff32fbdde69d29ef1f774f9c7d4f
zelekmarcin <marcininho@gmail.com>
2017-06-19 15:27:59 +0200

    Changed 2 points on coordinate axis

--- ZELEK/ZELEK/Program.cs
+++ ZELEK/ZELEK/Program.cs
@@ -7,23 +7,26 @@ using System.Threading.Tasks;
 namespace ZELEK
 {
 
-    class Punkty_dwa
-    {
-        public double X1, X2;
-        public double Y1, Y2;
-        public Punkty_dwa(double a1, double a2, double b1, double b2)
-        {
-            this.X1 = a1;
-            this.Y1 = b1;
-            this.X2 = a2;
-            this.Y2 = b2;
-        }
-    }
-}
 class Program
     {
         static void Main(string[] args)
         {
-        }
+        int X1, X2, Y1, Y2;
+            //współrzędne 1 punktu
+        Console.Write("Input the value for X1 coordinate :"); 
+        X1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the value for Y1 coordinate :");
+        Y1 = Convert.ToInt32(Console.ReadLine());
+
+            // współrzędne 2 punktu
+
+        Console.Write("Input the value for X2 coordinate :");
+        X2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the value for Y2 coordinate :");
+        Y2 = Convert.ToInt32(Console.ReadLine());
+
     }
+}
 }
