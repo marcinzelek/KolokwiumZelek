a503b635b218ddedb05bb4baeb1fc1d57eb27629
zelekmarcin <marcininho@gmail.com>
2017-06-19 15:42:48 +0200

    Did nothing. Again

--- ZELEK/ZELEK/Program.cs
+++ ZELEK/ZELEK/Program.cs
@@ -7,26 +7,26 @@ using System.Threading.Tasks;
 namespace ZELEK
 {
 
-class Program
+    class Program
     {
         static void Main(string[] args)
         {
-        int X1, X2, Y1, Y2;
+            int X1, X2, Y1, Y2;
             //współrzędne 1 punktu
-        Console.Write("Input the value for X1 coordinate :"); 
-        X1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the value for X1 coordinate :");
+            X1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Input the value for Y1 coordinate :");
-        Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the value for Y1 coordinate :");
+            Y1 = Convert.ToInt32(Console.ReadLine());
 
             // współrzędne 2 punktu
 
-        Console.Write("Input the value for X2 coordinate :");
-        X2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the value for X2 coordinate :");
+            X2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Input the value for Y2 coordinate :");
-        Y2 = Convert.ToInt32(Console.ReadLine());
-
+            Console.Write("Input the value for Y2 coordinate :");
+            Y2 = Convert.ToInt32(Console.ReadLine());
+        }
     }
 }
-}
+
